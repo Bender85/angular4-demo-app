50442b6438f019679e7bb756e01a37144ed9eeff
Nikita <nikitosik85@gmail.com>
2017-09-25 22:22:56 +0300

    wip component and databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  usename = 'DemoApp';
+  counter = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChangeName() {
+    this.usename = 'changed Name';
+  }
+
+  clear() {
+    this.usename = '';
+    this.counter = 0;
+  }
+  count() {
+    this.counter++;
   }
 
 }
